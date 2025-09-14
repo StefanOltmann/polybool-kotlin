bc01f031e8b0342013629962f1c5662287b6770a
Stefan Oltmann <github@stefan-oltmann.de>
2025-09-14 12:47:10 +0200

    Reworked SegmentSelector.kt

--- src/commonMain/kotlin/de/stefan_oltmann/polybool/internal/SegmentSelector.kt
+++ src/commonMain/kotlin/de/stefan_oltmann/polybool/internal/SegmentSelector.kt
@@ -15,27 +15,83 @@ import de.stefan_oltmann.polybool.models.Segment.SegmentFill
 
 internal object SegmentSelector {
 
-    private fun select(segments: List<Segment>, selection: IntArray): List<Segment> {
+    private val unionSelection = intArrayOf(
+        0, 2, 1, 0,
+        2, 2, 0, 0,
+        1, 0, 1, 0,
+        0, 0, 0, 0
+    )
+
+    private val intersectSelection = intArrayOf(
+        0, 0, 0, 0,
+        0, 2, 0, 2,
+        0, 0, 1, 1,
+        0, 2, 1, 0
+    )
+
+    private val differenceSelection = intArrayOf(
+        0, 0, 0, 0,
+        2, 0, 2, 0,
+        1, 1, 0, 0,
+        0, 1, 2, 0
+    )
+
+    private val differenceRevSelection = intArrayOf(
+        0, 2, 1, 0,
+        0, 0, 1, 1,
+        0, 2, 0, 2,
+        0, 0, 0, 0
+    )
+
+    private val xorSelection = intArrayOf(
+        0, 2, 1, 0,
+        2, 0, 0, 1,
+        1, 0, 0, 2,
+        0, 1, 2, 0
+    )
+
+    fun union(segments: List<Segment>): List<Segment> =
+        select(segments, unionSelection)
+
+    fun intersect(segments: List<Segment>): List<Segment> =
+        select(segments, intersectSelection)
+
+    fun difference(segments: List<Segment>): List<Segment> =
+        select(segments, differenceSelection)
+
+    fun differenceRev(segments: List<Segment>): List<Segment> =
+        select(segments, differenceRevSelection)
+
+    fun xor(segments: List<Segment>): List<Segment> =
+        select(segments, xorSelection)
+
+    private fun select(
+        segments: List<Segment>,
+        selection: IntArray
+    ): List<Segment> {
 
         val result: MutableList<Segment> = ArrayList<Segment>()
 
-        for (seg in segments) {
-            val otherFill = seg.otherFill
+        for (segment in segments) {
+
+            val otherFill = segment.otherFill
+
             val index =
-                (if (seg.myFill.above == true) 8 else 0) +
-                    (if (seg.myFill.below == true) 4 else 0) +
+                (if (segment.myFill.above == true) 8 else 0) +
+                    (if (segment.myFill.below == true) 4 else 0) +
                     (if (otherFill != null && otherFill.above == true) 2 else 0) +
                     (if (otherFill != null && otherFill.below == true) 1 else 0)
 
             if (selection[index] != 0) {
-                /* copy the segment to the results, while also calculating the fill status */
+
+                /* Copy the segment to the results, while also calculating the fill status */
                 result.add(
                     Segment(
-                        seg.start,
-                        seg.end,
-                        SegmentFill(
-                            selection[index] == 1,
-                            selection[index] == 2
+                        start = segment.start,
+                        end = segment.end,
+                        myFill = SegmentFill(
+                            above = selection[index] == 1,
+                            below = selection[index] == 2
                         )
                     )
                 )
@@ -43,154 +99,5 @@ internal object SegmentSelector {
         }
 
         return result
-    }
-
-
-    fun union(segments: List<Segment>): List<Segment> {
-        /*
-         above1 below1 above2 below2    Keep?               Value
-            0      0      0      0   =>   no                  0
-            0      0      0      1   =>   yes filled below    2
-            0      0      1      0   =>   yes filled above    1
-            0      0      1      1   =>   no                  0
-            0      1      0      0   =>   yes filled below    2
-            0      1      0      1   =>   yes filled below    2
-            0      1      1      0   =>   no                  0
-            0      1      1      1   =>   no                  0
-            1      0      0      0   =>   yes filled above    1
-            1      0      0      1   =>   no                  0
-            1      0      1      0   =>   yes filled above    1
-            1      0      1      1   =>   no                  0
-            1      1      0      0   =>   no                  0
-            1      1      0      1   =>   no                  0
-            1      1      1      0   =>   no                  0
-            1      1      1      1   =>   no                  0
-        */
-        return select(
-            segments, intArrayOf(
-                0, 2, 1, 0,
-                2, 2, 0, 0,
-                1, 0, 1, 0,
-                0, 0, 0, 0
-            )
-        )
-    }
-
-
-    fun intersect(segments: List<Segment>): List<Segment> {
-        /*
-         above1 below1 above2 below2    Keep?               Value
-            0      0      0      0   =>   no                  0
-            0      0      0      1   =>   no                  0
-            0      0      1      0   =>   no                  0
-            0      0      1      1   =>   no                  0
-            0      1      0      0   =>   no                  0
-            0      1      0      1   =>   yes filled below    2
-            0      1      1      0   =>   no                  0
-            0      1      1      1   =>   yes filled below    2
-            1      0      0      0   =>   no                  0
-            1      0      0      1   =>   no                  0
-            1      0      1      0   =>   yes filled above    1
-            1      0      1      1   =>   yes filled above    1
-            1      1      0      0   =>   no                  0
-            1      1      0      1   =>   yes filled below    2
-            1      1      1      0   =>   yes filled above    1
-            1      1      1      1   =>   no                  0
-        */
-        return select(
-            segments, intArrayOf(
-                0, 0, 0, 0,
-                0, 2, 0, 2,
-                0, 0, 1, 1,
-                0, 2, 1, 0
-            )
-        )
-    }
-
-
-    fun difference(segments: List<Segment>): List<Segment> { /* primary - secondary */
-        // above1 below1 above2 below2    Keep?               Value
-        //    0      0      0      0   =>   no                  0
-        //    0      0      0      1   =>   no                  0
-        //    0      0      1      0   =>   no                  0
-        //    0      0      1      1   =>   no                  0
-        //    0      1      0      0   =>   yes filled below    2
-        //    0      1      0      1   =>   no                  0
-        //    0      1      1      0   =>   yes filled below    2
-        //    0      1      1      1   =>   no                  0
-        //    1      0      0      0   =>   yes filled above    1
-        //    1      0      0      1   =>   yes filled above    1
-        //    1      0      1      0   =>   no                  0
-        //    1      0      1      1   =>   no                  0
-        //    1      1      0      0   =>   no                  0
-        //    1      1      0      1   =>   yes filled above    1
-        //    1      1      1      0   =>   yes filled below    2
-        //    1      1      1      1   =>   no                  0
-        return select(
-            segments, intArrayOf(
-                0, 0, 0, 0,
-                2, 0, 2, 0,
-                1, 1, 0, 0,
-                0, 1, 2, 0
-            )
-        )
-    }
-
-
-    fun differenceRev(segments: List<Segment>): List<Segment> { // secondary - primary
-        // above1 below1 above2 below2    Keep?               Value
-        //    0      0      0      0   =>   no                  0
-        //    0      0      0      1   =>   yes filled below    2
-        //    0      0      1      0   =>   yes filled above    1
-        //    0      0      1      1   =>   no                  0
-        //    0      1      0      0   =>   no                  0
-        //    0      1      0      1   =>   no                  0
-        //    0      1      1      0   =>   yes filled above    1
-        //    0      1      1      1   =>   yes filled above    1
-        //    1      0      0      0   =>   no                  0
-        //    1      0      0      1   =>   yes filled below    2
-        //    1      0      1      0   =>   no                  0
-        //    1      0      1      1   =>   yes filled below    2
-        //    1      1      0      0   =>   no                  0
-        //    1      1      0      1   =>   no                  0
-        //    1      1      1      0   =>   no                  0
-        //    1      1      1      1   =>   no                  0
-        return select(
-            segments, intArrayOf(
-                0, 2, 1, 0,
-                0, 0, 1, 1,
-                0, 2, 0, 2,
-                0, 0, 0, 0
-            )
-        )
-    }
-
-
-    fun xor(segments: List<Segment>): List<Segment> { // primary ^ secondary
-        // above1 below1 above2 below2    Keep?               Value
-        //    0      0      0      0   =>   no                  0
-        //    0      0      0      1   =>   yes filled below    2
-        //    0      0      1      0   =>   yes filled above    1
-        //    0      0      1      1   =>   no                  0
-        //    0      1      0      0   =>   yes filled below    2
-        //    0      1      0      1   =>   no                  0
-        //    0      1      1      0   =>   no                  0
-        //    0      1      1      1   =>   yes filled above    1
-        //    1      0      0      0   =>   yes filled above    1
-        //    1      0      0      1   =>   no                  0
-        //    1      0      1      0   =>   no                  0
-        //    1      0      1      1   =>   yes filled below    2
-        //    1      1      0      0   =>   no                  0
-        //    1      1      0      1   =>   yes filled above    1
-        //    1      1      1      0   =>   yes filled below    2
-        //    1      1      1      1   =>   no                  0
-        return select(
-            segments, intArrayOf(
-                0, 2, 1, 0,
-                2, 0, 0, 1,
-                1, 0, 0, 2,
-                0, 1, 2, 0
-            )
-        )
     }
 }
